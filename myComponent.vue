78c20f0166bcf0668c4dc6aa549cb076e197d1ba
Pulipuli Chen <pulipuli.chen@gmail.com>
2022-07-30 21:52:37 +0800

    咦，為什麼有這個？

---
+++ myComponent.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref, reactive, onMounted } from 'vue'
+
+const ary1 = reactive([])
+
+async function fetchList(url) {
+  let response = await fetch(url)
+  let text = await response.text()
+  return text.trim().split('\n')
+}
+
+//onMounted(() => window.addEventListener('mousemove', update))
+//onUnmounted(() => window.removeEventListener('mousemove', update))
+onMounted(async () => {
+  ary1.push(...(await fetchList('主詞.txt')))
+
+  // ary1.push('aaas')
+
+  //console.log(ary1Response)
+  //let ary1 = ary1Response.text().split()
+  // console.log(ary1)
+})
+</script>
+
+<template>
+{{ ary1 }}
+</template>
